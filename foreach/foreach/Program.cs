dfef5297d25323015a29bf96dddd14d8e827b192
Naman9058 <99949580+Naman9058@users.noreply.github.com>
2022-03-03 22:36:38 +0530

    Add files via upload

---
+++ foreach/foreach/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace app
+{
+    class Program
+    {
+        static void Main(string[]arg)
+        {
+            int[] num = { 1, 2,3, 4, 5, 6, 7, 8, 9, 10,};
+            Console.WriteLine(num.Max());
+            for (int i = 1; i < num.Length; i++)
+            {
+                if (i == 5)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
